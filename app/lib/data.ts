227c4a1ee914fdd2cbb6f9892ff2324264994b5c
Allan Gallo <allan.gallo.guerra@gmail.com>
2024-01-24 23:16:09 -0600

    Chapter 11 completed
    use debounce package
    pagination
    table
    query params
    create new url from the search input

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -141,6 +141,7 @@ export async function fetchInvoicesPages(query: string) {
   `;
 
     const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
+    console.log(totalPages);
     return totalPages;
   } catch (error) {
     console.error('Database Error:', error);
